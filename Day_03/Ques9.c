32bb86c15d841eb1f9d9b338a5c195bb99c5a1ff
Thicken-Chanduri <shreyansh24w@gmail.com>
2026-06-30 01:18:07 +0530

    Write a program to Check whether a number is prime.
    Fix typos in printf and loop condition, correct prime checking logic.

---
+++ Day_03/Ques9.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printff("Enter the number");    
+    scanf("%d",&a);
+    for(b=2;b,a/2;b++)
+    {
+        if(a%b==0)
+        {
+            printf("%d is not a prime number",a);
+            break;
+        }
+        else{
+            pritnf("%d is a prime number",a);
+            break;
+        }
+        return 0;
+    }
+
+}
