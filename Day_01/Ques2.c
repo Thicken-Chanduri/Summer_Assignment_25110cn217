e3d897e782d54f7fb16903a4372514f549c84259
Thicken-Chanduri <shreyansh24w@gmail.com>
2026-06-30 01:05:48 +0530

    Write a program to Print multiplication table of a given number

---
+++ Day_01/Ques2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+{
+    int a,b;
+    printf("Enter the value of number:");
+    scanf("%d",&a);
+    for (b=1;b<=10;b++)
+    {
+        printf("%d x %d= %d\n",a,b,a*b);
+    
+    }
+    return 0;
+
+}
