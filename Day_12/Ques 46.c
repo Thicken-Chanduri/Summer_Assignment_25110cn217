520e1c2e648ea56f254eecc31e1b938e975ce009
Thicken-Chanduri <shreyansh24w@gmail.com>
2026-07-19 10:45:00 +0530

    Add Armstrong number checker

---
+++ Day_12/Ques 46.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int isArmstrong(int n) {
+    int original = n, rem, sum = 0;
+
+    while (n > 0) {
+        rem = n % 10;
+        sum = sum + (rem * rem * rem);
+        n = n / 10;
+    }
+
+    if (sum == original)
+        return 1;
+    else
+        return 0;
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isArmstrong(num))
+        printf("%d is an Armstrong number.\n", num);
+    else
+        printf("%d is not an Armstrong number.\n", num);
+
+    return 0;
+}
