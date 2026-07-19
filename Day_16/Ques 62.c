0ae1a228dbfc4728335fd872562b35ec9ed7c4a8
Thicken-Chanduri <shreyansh24w@gmail.com>
2026-07-19 11:07:57 +0530

    Add program to find element with maximum frequency
    This program finds the element with the maximum frequency in an array and prints it along with its frequency.

--- Day_16/Ques 62.c
+++ Day_16/Ques 62.c
@@ -1,1 +1,33 @@
 
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, j;
+    int maxCount = 0, count, element;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for(i = 0; i < n; i++) {
+        count = 1;
+
+        for(j = i + 1; j < n; j++) {
+            if(arr[i] == arr[j])
+                count++;
+        }
+
+        if(count > maxCount) {
+            maxCount = count;
+            element = arr[i];
+        }
+    }
+
+    printf("Element with Maximum Frequency = %d\n", element);
+    printf("Frequency = %d", maxCount);
+
+    return 0;
+}
